16ac336b1de58a58461e55591283caa9aad70db8
Hammad Ahmad <hammadahmad@192.168.10.50>
2023-09-16 13:38:12 +0500

    complete google oauth2 setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'static_pages/home'
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  root 'static_pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
 end
