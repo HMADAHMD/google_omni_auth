16e9fbc19689d40a8756535e070e6786511b0e69
Hammad Ahmad <hammadahmad@192.168.10.50>
2023-09-16 21:50:58 +0500

    add listing model update associations

---
+++ app/controllers/host/listings_controller.rb
@@ -0,0 +1,63 @@
+class Host::ListingsController < ApplicationController
+  #before_action :authenticate_user!
+
+  def index
+    @listing = current_user.listings
+  end
+
+  def new
+    @listing = Listing.new
+  end
+
+  def create
+    @listing = current_user.listings.new(listing_create_params)
+    if @listing.save
+      redirect_to hosts_listing_path(@listing)
+    else
+      flash.now[:errors] = @listing.errors.full_messages
+      render :new
+    end
+  end
+
+  def update
+    @listing = current_user.listing.find(params[:id])
+    if @listing.update(listing_update_params)
+      redirect_to hosts_listing_path(@listing)
+    else
+      flash.now[:errors] = @listing.errors.full_messages
+      render :new
+    end
+  end
+
+  def edit
+    @listing = current_user.listings.find(params[:id])
+  end
+
+  def show
+    @listing = current_user.listing.find(params[:id])
+  end
+
+  def destroy
+    @listing = current_user.listing.find(params[:id])
+    if @listing.present?
+      @listing.update(status: :archived)
+      redirect_to hosts_listings_path
+    else
+      flash.now[:errors] = @listing.errors.full_messages
+      render :new
+    end
+  end
+
+  private
+
+  def listing_create_params
+    params.require(:listing).permit(:title, :about, :max_guests, :address_line_1, :address_line_2, :city, :state, :postal_code, :country)
+  end
+
+  def listing_update_params
+    params.require(:listing).permit(:title, :about, :max_guests)
+  end
+
+  def set_listing
+  end
+end
