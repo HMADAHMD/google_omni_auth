8f6a36bdcfcc5b6de15b0a44297f8ae2feaf5d96
HMADAHMD <hammadja01@gmail.com>
2023-09-18 23:09:18 +0500

    maps feature will integrate later as I have to set billing to my google account

---
+++ app/javascript/controllers/address_controller.js
@@ -0,0 +1,16 @@
+import {Controller} from '@hotwired/stimulus'
+
+export default class extends Controller {
+    connect() {
+        console.log("Address controller is connected.");
+    }
+
+
+    initGoogle() {
+        
+    }
+
+    placeSelected() {
+
+    }
+}
